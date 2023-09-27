ee08a03b2d3c20da0f9a4b2ac1aa0fb48bffda4d
Claudio Cruz <claudiocruz1989@gmail.com>
2023-09-27 03:14:20 +0000

    Create Expenses.js and Card.js with the respective css files

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,15 +1,18 @@
-import ExpenseDate from './ExpenseDate';
-import '../Style/ExpenseItemStyle.css';
+import React from 'react';
 
-function ExpenseItem(props) {
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import '../../Style/ExpenseItemStyle.css';
+
+const ExpenseItem = (props) => {
   return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
